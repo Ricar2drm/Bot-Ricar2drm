569c7f3e9469aa3fbd713b323035286ea62e1365
Ricar2drm <87620981+Ricar2drm@users.noreply.github.com>
2021-10-13 23:27:56 +0200

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,31 +1,10 @@
 const version = (prefix, pushname) => {
     return `
-*ѕнαηвσт ву ѕнαη∂υу*
-
-*Actualizado:* 18 de Septiembre del 2021
-*Versión actual:* 4.0
-*Ofrecida por:* shanduy™
+*Versión del bot:* la misma que ayer
 
 
-*INFORME*
 
-Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
-Cambiare la apikey pasando un mes o si veo muchos atercados o robos
-Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
-
-
-*COMO ACTUALIZAR EL BOT*
-
-https://cutt.ly/dWp5uqE
-
-
-*SIGUEME EN INSTAGRAM 🥸🧉*
-
-https://www.instagram.com/thepavos
-
-
-_*by shanduy*_
+_*Ricar2drm*_
 `
 
 }
